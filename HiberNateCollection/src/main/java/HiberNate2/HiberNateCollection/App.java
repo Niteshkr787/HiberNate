ecdb6a001c5e95d54ca0037cedbcab2a6add6276
nitesh <niteshkr.gupta787@gmail.com>
2023-08-05 13:42:42 +0530

    CollectionType

---
+++ HiberNateCollection/src/main/java/HiberNate2/HiberNateCollection/App.java
@@ -0,0 +1,14 @@
+package HiberNate2.HiberNateCollection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       
+    }
+}
